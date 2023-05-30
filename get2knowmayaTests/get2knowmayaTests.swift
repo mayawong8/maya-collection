1632de36ab0eb0794811eba916f9a51c79398d94
Maya <mayajwong8@gmail.com>
2023-05-30 10:45:13 -0400

    inital commit

---
+++ get2knowmayaTests/get2knowmayaTests.swift
@@ -0,0 +1,33 @@
+//
+//  get2knowmayaTests.swift
+//  get2knowmayaTests
+//
+//  Created by Maya Wong on 2023-05-30.
+//
+
+import XCTest
+@testable import get2knowmaya
+
+class get2knowmayaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
